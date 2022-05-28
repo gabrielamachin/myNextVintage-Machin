fb77b5f10ca66b20f961dd85d4be99b994b92599
Gabriela Machin <gabrielamachin.gm@gmail.com>
2022-05-27 22:18:39 -0300

    Actividad sobre componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <ItemListContainer greeting='Bienvenidos a MyNextVintage' />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <button>Remeras y camisas</button>
+            <button>Pantalones</button>
+            <button>Abrigos</button>
+            <button>Faldas y vestidos</button>
+            <button>Accesorios</button>
+            <button>Liquidación</button>
+        </nav>
+    )
+}
+
+export default NavBar
